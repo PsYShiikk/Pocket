1a3773c4b285529d88db2f215014fd2a7e919044
leo.delplanque <delplanqueleomail.com>
2020-01-17 17:05:48 +0100

    PAGE DACCUEIL ET LIEN VERS SOUS MENU

--- app/src/main/java/com/example/leodelplanque/pocket/accueil.java
+++ app/src/main/java/com/example/leodelplanque/pocket/accueil.java
@@ -1,7 +1,9 @@
 package com.example.leodelplanque.pocket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class accueil extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class accueil extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+    }
+
+    public void audiovisuelButton(View View){
+        startActivity(new Intent(this,audiovisuelMenu.class));
+
+    }
+
+    public void communicationButton(View View){
+        startActivity(new Intent(this,communicationMenu.class));
+
+    }
+    public void webButton(View View){
+        startActivity(new Intent(this,webMenu.class));
+
     }
 }
---
+++ app/src/main/java/com/example/leodelplanque/pocket/audiovisuelMenu.java
@@ -0,0 +1,18 @@
+package com.example.leodelplanque.pocket;
+
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+
+public class audiovisuelMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audiovisuel);
+    }
+
+
+}
+
