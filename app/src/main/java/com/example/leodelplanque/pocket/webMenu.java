8d7ee867623983a7b9901729046379902aead69b
Gokoura <dumontdd59@gmail.com>
2020-02-13 09:03:45 +0100

    MAJ DENIS HEX BINAIRE DECIMAL

--- app/src/main/java/com/example/leodelplanque/pocket/webMenu.java
+++ app/src/main/java/com/example/leodelplanque/pocket/webMenu.java
@@ -1,7 +1,9 @@
 package com.example.leodelplanque.pocket;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class webMenu extends AppCompatActivity {
 
@@ -10,4 +12,18 @@ public class webMenu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_menu);
     }
+
+    public void hex_bin_dec_Button(View View){
+        startActivity(new Intent(this,hex_bin_dec.class));
+
+    }
+
+    public void hex_rgb_Button(View View){
+        startActivity(new Intent(this,hex_rgb.class));
+
+    }
 }
+
+
+
+
